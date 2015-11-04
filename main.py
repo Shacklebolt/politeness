309b2ff983ad76d051b45e9a9af3b4edf6a2be62
Muhammad Ali <mohummed.alee@gmail.com>
2015-11-04 22:53:26 +0500

    This configuration reports 78.7%

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
     np.random.seed(43)
     # hyper parameters to be tweaked here
     load_rnn_from_pickle = False
-    training_size = 2050  # maximum of 2113, 2050 requests when Ali ran this (September 23, 2015)
-    l_rate = 0.01
+    training_size = 2109  # maximum of 2113, 2050 requests when Ali ran this (September 23, 2015)
+    l_rate = 0.001
     mini_batch_size = 20
     reg_cost = 0.001
     epochs = 100
-    activation_func = "sig"
+    activation_func = "tanh"
     dim = 100
     ######################################
 
